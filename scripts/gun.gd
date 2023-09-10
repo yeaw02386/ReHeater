acd253e2512819552ab067a0b4df1ff3b2b76f5b
yeaw02386 <yeaw02386@gmail.com>
2023-09-10 14:47:38 +0700

    bullet swaping

---
+++ scripts/gun.gd
@@ -0,0 +1,43 @@
+extends Node2D
+@export var bulletLight:PackedScene
+@export var bulletAOE:PackedScene
+@export var bulletPiercing:PackedScene
+var allBullet
+var bullet 
+
+var delayMetaData = {0:0.2,
+					 1:1.2,
+					 2:0.7}
+var nowBulletType = -1
+
+func _ready():
+	allBullet = [bulletLight,bulletAOE,bulletPiercing]
+	swapBullet() 
+
+func _process(delta):
+	pass
+
+func shoot() :
+	var ins = bullet.instantiate()
+	var pos = $Polygon2D.position + get_parent().position + position
+	ins.init(pos)
+	get_parent().get_parent().add_child(ins)
+	get_tree().call_group("heat","on_heating",ins.heatGening)
+	print(ins)
+
+func _on_shoot_delay_timeout():
+	if Input.is_action_pressed("attack"):
+		shoot()
+		
+func _input(event):
+	if event.is_action_pressed("swapBullet") :
+		swapBullet()
+		
+func swapBullet():
+	nowBulletType += 1
+	if nowBulletType == 3 :
+		nowBulletType = 0
+	
+	bullet = allBullet[nowBulletType]
+	$shootDelay.wait_time = delayMetaData.get(nowBulletType)
+	
--- scripts/ship.gd
+++ scripts/ship.gd
@@ -3,6 +3,7 @@ extends Node2D
 var MAX_HEAT = 1000
 var heat = 30
 var coolerDMG = 1
+var heatLimit = []
 
 func _ready():
 	pass
@@ -14,10 +15,14 @@ func on_heating(temp) :
 	heat += temp*coolerDMG
 	if heat <= MAX_HEAT :
 		get_tree().call_group("heat","on_destroy")
-	
-	print(heat)
 
 func on_cooling(temp) :
 	heat -= temp
 
+func _input(event):
+	if event.is_action_pressed("aoeGun") :
+		pass
+		
+func on_setHeatLimit(temp) :
+	heatLimit.append(temp)
 
